dafb84e4642577b729fe79256374a50973f86c8b
techzik323 <techzik323@gmail.com>
2025-02-09 00:09:40 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -8,7 +8,7 @@ const chalk = require('chalk')
 if (fs.existsSync('.env')) require('dotenv').config({ path: __dirname+'/.env' })
 
 
-global.SESSION_ID = process.env.SESSION_ID || 'Bellah~2z40RA7T#VRneuCjwFxSYHupVJSJHZDNmNDE_THLCNxjVd9jGWuw' 
+global.SESSION_ID = process.env.SESSION_ID || 'Bellah~ujwk1LBD#N222NzW_DqDJgGsZfE6Emh6eFFJXpBxSK2wRXgatUe4' 
 //owmner v card
 global.ytname = "YT: GIDDYTENNOR" //ur yt chanel name
 global.socialm = "IG: @GIDDYTENNOR" //ur github or insta name
@@ -16,10 +16,10 @@ global.location = "Kenya" //ur location
 
 //new
 global.botname = process.env.BOT_NAME ||'𝐁𝐞𝐥𝐥𝐚𝐡 𝐗𝐦𝐝' //enter your  bot name here
-global.ownernumber = process.env.OWNER_NUMBER ||'254104245659' //ur owner number
-global.ownername = '© TENNOMODZ' //ur owner name
-global.websitex = "https://whatsapp.com/channel/0029VaPZWbY1iUxVVRIIOm0D" //"https://chat.whatsapp.com/JmsgzJllAAB8zHfQcJXxES"
-global.wagc = "https://chat.whatsapp.com/CzFlFQrkdzxFw0pxCBYM7H" //"https://chat.whatsapp.com/JmsgzJllAAB8zHfQcJXxES"
+global.ownernumber = process.env.OWNER_NUMBER ||'256751617788' //ur owner number
+global.ownername = 'ZIK' //ur owner name
+global.websitex = "https://chat.whatsapp.com/Hty9A7GOfG75nIftDtnuz5" //"https://chat.whatsapp.com/JmsgzJllAAB8zHfQcJXxES"
+global.wagc = "https://chat.whatsapp.com/Hty9A7GOfG75nIftDtnuz5" //"https://chat.whatsapp.com/JmsgzJllAAB8zHfQcJXxES"
 global.themeemoji = '🪀'
 global.wm = "GIDDY TENNOR"
 global.botscript = 'https://whatsapp.com/channel/0029VaPZWbY1iUxVVRIIOm0D' //'https://chat.whatsapp.com/JmsgzJllAAB8zHfQcJXxES' //script link
@@ -27,22 +27,22 @@ global.packname = process.env.PACK_NAME ||"BELLAH XMD" //enter your stickers aut
 global.author = "Giddy-Tennor"
 global.creator = "254703726139@s.whatsapp.net"
 global.xprefix = process.env.PREFIX ||'.'
-global.premium = ["254703726139"] // Premium User
+global.premium = ["256751617788"] // Premium User
 global.hituet = 0
 
 //bot sett
 global.typemenu = process.env.TYPE_MENU || 'v2' // menu type 'v1' => 'v8' 
 global.typereply = process.env.TYPE_REPLY ||'v1' // reply type 'v1' => 'v3'
 global.autoblocknumber = process.env.AUTOBLOCK_NUMBER || '263,234' //set autoblock country code
-global.antiforeignnumber = process.env.ANTIFOREIGN_NUMBER || '' //set anti foreign number country code
-global.welcome = process.env.WELCOME || 'false' //welcome/left in groups
+global.antiforeignnumber = process.env.ANTIFOREIGN_NUMBER || '92' //set anti foreign number country code
+global.welcome = process.env.WELCOME || 'true' //welcome/left in groups
 global.anticall = process.env.ANTI_CALL || 'false' //bot blocks user when called
 global.autoswview = process.env.AUTOSW_VIEW || 'true' //auto status/story view
 global.adminevent = true //show promote/demote message
 global.groupevent = true //show update messages in group chat
 //msg
-global.autorecord = process.env.AUTO_RECORD || 'false'
-global.autorecordtype = process.env.AUTO_RECORDTYPE || 'false'
+global.autorecord = process.env.AUTO_RECORD || 'true'
+global.autorecordtype = process.env.AUTO_RECORDTYPE || 'true'
 
 global.mess = {
 	limit: 'Your limit is up <\>',
